c2584c69beead85035cd55847b5081d5778a5f3f
mvirdone1 <mvirdone1@users.noreply.github.com>
2022-08-22 06:39:59 -0600

    Update workshop.js
    Adding more prompts

--- workshop.js
+++ workshop.js
@@ -4,26 +4,31 @@ let coaching = [
     "Partner Prompt",
     "Coach Prompt"],
         
-    ["Coaching on a new opportunity",
+    ["Coaching on a new Opportunity",
     "Parnter - You are consideinrg applying for a promotion (or a new role), but you are not confident in your abilities. The coach is a friend of yours, and the coach initiates conversation",
-    "Coach - You start a conversation with a friend/peer about work 'how are things at work' and they seem to be struggling with something, coach speaks first"],
+    "Coach - You start a conversation with a friend/peer about work 'how are things at work' and they seem to be struggling with something."],
 
     ["Challenging Teammate",
-    "Partner - You have a teammate who is hard to work with because they [don't deliver, are rude or communicate ineffectively, get distracted with unimportant work] and you're sick of it, you are about to have a 1:1 with your manager",
-    "Coach - You are a manager and are having a 1:1 with your teammate, you ask how their current project is going (coach speaks first) "],
+    "Partner - You have a teammate who is hard to work with because they [don't deliver, are rude or communicate ineffectively, get distracted with unimportant work] and you're sick of it, you are about to have a 1:1 with your manager. (Unspoken context - You have not tried to address the issue with the challenging teammate)",
+    "Coach - You are a manager and are having a 1:1 with your teammate, you ask how their current project is going."],
 
     
-    ["Perceived favoritism",
-    "Partner - You are frustrated because your tech lead/manager on your product team always picks <other person>. (Unspoken subtext, this other person is actually the most qualified, but you're struggling with how to grow technically)",
-    "Coach - You are having a chat with a teammate and they seem unhappy, ask an opening question and see where the conversation takes you"],
+    ["Perceived Favoritism",
+    "Partner - You are frustrated because your tech lead/manager on your product team always picks <other person> for tasks you would like to be assigned. (Unspoken subtext, this other person is actually the most qualified, but you're struggling with how to grow technically)",
+    "Coach - You are having a chat with a teammate/peer and they seem unhappy, ask an opening question and see where the conversation takes you"],
 
-    ["Divergence from practices",
-    "Partner - You are working with a challenging client and are feeling some pressure to deliver, this has caused you to take some shortcuts",
-    "Coach - You have noticed one of your teammates/peers on an engagement isn't quite delivering in the way we operate at Labs, you want to address the issues, you speak first"],
+    ["Divergence from Practices",
+    "Partner - You are working with a challenging client and are feeling some pressure to deliver, this has caused you to take some shortcuts, use an example from your professional realm to make the dialog more realistic",
+    "Coach - You have noticed one of your teammates/peers on an engagement isn't quite delivering in the way we operate at Labs, you want to address the issues"],
 
     ["Ignoring user research",
     "Partner - You have strong opinions on which features are most important for the product, however recent user interviews don't agree, you want to push those user requests to the next iteration and work on your personal priority first",
     "Coach - You noticed one of your teammates seems to be diverging from what we heard at the most recent batch of user interview, ask an opening question and try and figure out how to get back on track"],
+
+
+    ["Challenging Technical Issue (Stuck)",
+    "You are dealing wtih a challenging technical issue (use something relevant to your field) and feel stuck, you need someone to help you explore other options. You are about to sit down with your tech lead or manager to have a discussion.",
+    "You are a manager or tech lead and are about to have an informal chat with one of your teammates on your project. Ask an opening question and see where things take you"]
 
     ["Scenario 3",
     "asdfasdfasdf",
@@ -86,5 +91,7 @@ function popOptions()
         el.value = i;
         select.appendChild(el);
     }
+
+    updateWorkshop()
 
 }

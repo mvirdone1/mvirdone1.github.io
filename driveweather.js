930735743081156727c3a26de3143ca7d9fed001
mvirdone1 <mvirdone@gmail.com>
2023-11-09 05:46:58 -0700

    Got rid of the text field for global variable

--- driveweather.js
+++ driveweather.js
@@ -1,86 +1,44 @@
-// var myGMap;
-
-function showMapDrive() {
-  console.log("In Show Map");
-
-  let lat = 40;
-  let lon = -110;
-
-  /* const map = new google.maps.Map($("map"), {
-      center: { lat: lat, lng: lon },
-      zoom: 5,
-    });
-*/
-  const myGMap = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: lat, lng: lon },
-    zoom: 5,
-  });
-
-  var marker;
-
-  myGMap.addListener("click", (event) => {
-    lat = Math.round(event.latLng.lat() * 100) / 100;
-    lon = Math.round(event.latLng.lng() * 100) / 100;
-
-    appendLatLon(lat, lon);
-
-    // Remove the previous marker if it exists
-    if (marker) {
-      marker.setMap(null);
-    }
-
-    // Add a new marker to show the most recent click
-    marker = new google.maps.Marker({
-      position: event.latLng,
-      map: myGMap,
-    });
-    updateDriveList();
-  });
+class DriveWeather {
+  constructor(mapId, initialCenter, initialZoom) {}
 }
+
+let locationList = [];
 
 function appendLatLon(lat, lon) {
-  oldValue = document.getElementById("lat-lon").value;
-  document.getElementById("lat-lon").value = oldValue + lat + "," + lon + ";";
-}
-
-function initMapDrive() {
-  console.log("Init Map Callback from Google");
-  showMapDrive();
+  locationList.push([lat, lon]);
 }
 
 function updateDriveList() {
-  driveLocations = document.getElementById("lat-lon").value.split(";");
-  // Clear the inner HTML every time we refresh the list
   $("#dynamic-div").empty();
 
-  linkURL =
-    "https://mvirdone1.github.io/driveweather.html?drive=" +
-    document.getElementById("lat-lon").value;
-  $("#drive-link").attr("href", linkURL);
+  linkURL = "https://mvirdone1.github.io/driveweather.html?drive=";
 
   myMapManager.deleteAllMarkers();
 
-  for (idx = 0; idx < driveLocations.length - 1; idx++) {
-    console.log("[" + idx + "]: " + driveLocations[idx]);
-    coordinates = driveLocations[idx].split(",");
-    getWeatherOverview(coordinates[0], coordinates[1], idx);
-    mapPosition = { lat: Number(coordinates[0]), lng: Number(coordinates[1]) };
+  for (idx = 0; idx < locationList.length; idx++) {
+    console.log("[" + idx + "]: " + locationList[idx]);
+
+    lat = locationList[idx][0];
+    lon = locationList[idx][1];
+
+    getWeatherOverview(lat, lon, idx);
+    mapPosition = {
+      lat: Number(lat),
+      lng: Number(lon),
+    };
     myMapManager.addMarker(mapPosition, "");
+
+    linkURL = linkURL + lat + "," + lon + ";";
   }
+
+  $("#drive-link").attr("href", linkURL);
 
   updateWeatherImages();
 }
 
 function deleteLocation(deleteIdx) {
-  driveLocations = document.getElementById("lat-lon").value.split(";");
+  locationList.splice(deleteIdx, 1);
 
-  $("#lat-lon").val("");
-  for (idx = 0; idx < driveLocations.length - 1; idx++) {
-    if (idx != deleteIdx) {
-      coordinates = driveLocations[idx].split(",");
-      appendLatLon(coordinates[0], coordinates[1]);
-    }
-  }
   updateDriveList();
 }
 
@@ -177,24 +135,6 @@ function initMapManager() {
 }
 
 function driveWeatherInit() {
-  // Setup the map pane
-  /*
-  const mapHTML = `    <div id="map" style="width: 75%; height: 400px;"></div>
-    <form>
-      <label for="lat-lon">Lat Lon List:</label>
-      <input type="text" id="lat-lon" name="lat-lon" />
-    </form>
-    <br>
-    <button id="addMarkerButton">Add Marker</button>
-    <button id="updateMarkerButton">Update Marker</button>
-    <button id="deleteMarkerButton">Delete Marker</button>
-    <a id="drive-link" href="">Linkable Version</a>
-    `;
-
-  const forecastElement = document.getElementById("map-div");
-  forecastElement.innerHTML = mapHTML;
-  */
-
   var reverseButton = $('<button id="reverseButton">Reverse List</button>');
 
   $("#map-div").append(reverseButton);
@@ -223,14 +163,7 @@ function driveWeatherInit() {
 }
 
 function reverseList() {
-  driveLocations = document.getElementById("lat-lon").value.split(";");
-
-  $("#lat-lon").val("");
-
-  for (idx = 0; idx < driveLocations.length - 1; idx++) {
-    coordinates = driveLocations[driveLocations.length - 2 - idx].split(",");
-    appendLatLon(coordinates[0], coordinates[1]);
-  }
+  locationList.reverse();
   updateDriveList();
 }
 
